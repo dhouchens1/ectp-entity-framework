d81832287fd4551e6c46ece296ac05b3854f26d5
David Houchens <david.houchens@lakeshore.com>
2023-07-06 11:50:36 -0400

    Added context

---
+++ ECTPEntityFramework/DataAccess/EctpContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using ECTPEntityFramework.Entities;
+
+namespace ECTPEntityFramework.DataAccess
+{
+    public class EctpContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+    }
+}

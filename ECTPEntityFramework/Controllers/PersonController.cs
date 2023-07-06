acffda3f5035c52dcc40b1e5384121d55bb8efd0
David Houchens <david.houchens@lakeshore.com>
2023-07-06 14:03:24 -0400

    Got everything wired up

--- ECTPEntityFramework/Controllers/PersonController.cs
+++ ECTPEntityFramework/Controllers/PersonController.cs
@@ -1,73 +1,36 @@
-﻿using System;
+﻿using System.Linq;
 using System.Net;
 using System.Web.Mvc;
 using ECTPEntityFramework.DataAccess;
 using ECTPEntityFramework.Entities;
-using PagedList;
 
 namespace ECTPEntityFramework.Controllers
 {
     public class PersonController : Controller
     {
-        private EctpContext db = new EctpContext();
+        private readonly EctpContext db = new EctpContext();
 
-        // GET: Student
-        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        public ViewResult Index(string currentFilter, string searchString)
         {
-            ViewBag.CurrentSort = sortOrder;
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewBag.CurrentFilter = currentFilter;
 
-            if (searchString != null)
+            var people = db.People.AsQueryable();
+            if (!string.IsNullOrEmpty(searchString))
             {
-                page = 1;
-            }
-            else
-            {
-                searchString = currentFilter;
+                people = people.Where(s => s.LastName.Contains(searchString)
+                                           || s.FirstName.Contains(searchString));
             }
 
-            ViewBag.CurrentFilter = searchString;
-
-            var people = db.People;
-
-            //var students = from s in db.Students
-            //               select s;
-            //if (!String.IsNullOrEmpty(searchString))
-            //{
-            //    students = students.Where(s => s.LastName.Contains(searchString)
-            //                           || s.FirstMidName.Contains(searchString));
-            //}
-            //switch (sortOrder)
-            //{
-            //    case "name_desc":
-            //        students = students.OrderByDescending(s => s.LastName);
-            //        break;
-            //    case "Date":
-            //        students = students.OrderBy(s => s.EnrollmentDate);
-            //        break;
-            //    case "date_desc":
-            //        students = students.OrderByDescending(s => s.EnrollmentDate);
-            //        break;
-            //    default:  // Name ascending 
-            //        students = students.OrderBy(s => s.LastName);
-            //        break;
-            //}
-
-            int pageSize = 3;
-            int pageNumber = (page ?? 1);
-            return View(people.ToPagedList(pageNumber, pageSize));
+            return View(people.ToList());
         }
 
-
-        // GET: Student/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //Student student = db.Students.Find(id);
+
             var person = db.People.Find(id);
             if (person == null)
             {
@@ -76,38 +39,25 @@ namespace ECTPEntityFramework.Controllers
             return View(person);
         }
 
-        // GET: Student/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Student/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate")]Person student)
+        public ActionResult Create([Bind(Include = "LastName, FirstName, BirthDate")]Person person)
         {
-            //try
-            //{
-                if (ModelState.IsValid)
-                {
-                    //db.Students.Add(student);
-                    //db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            //}
-            //catch (RetryLimitExceededException /* dex */)
-            //{
-            //    //Log the error (uncomment dex variable name and add a line here to write a log.
-            //    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-            //}
-            return View(student);
+            if (ModelState.IsValid)
+            {
+                db.People.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
         }
 
-
-        // GET: Student/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -115,18 +65,14 @@ namespace ECTPEntityFramework.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            var student = new Person();
-            //Student student = db.Students.Find(id);
-            if (student == null)
+            var person = db.People.Find(id);
+            if (person == null)
             {
                 return HttpNotFound();
             }
-            return View(student);
+            return View(person);
         }
 
-        // POST: Student/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
         public ActionResult EditPost(int? id)
@@ -135,26 +81,19 @@ namespace ECTPEntityFramework.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //var studentToUpdate = db.Students.Find(id);
-            //if (TryUpdateModel(studentToUpdate, "",
-            //   new string[] { "LastName", "FirstMidName", "EnrollmentDate" }))
-            //{
-            //    try
-            //    {
-            //        //db.SaveChanges();
 
-            //        return RedirectToAction("Index");
-            //    }
-            //    catch (RetryLimitExceededException /* dex */)
-            //    {
-            //        //Log the error (uncomment dex variable name and add a line here to write a log.
-            //        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
-            //    }
-            //}
+            var personToUpdate = db.People.Find(id);
+            if (TryUpdateModel(personToUpdate, "",
+                    new string[] { "LastName", "FirstName", "BirthDate" }))
+            {
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
             return View(new Person());
         }
 
-        // GET: Student/Delete/5
         public ActionResult Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null)
@@ -165,31 +104,31 @@ namespace ECTPEntityFramework.Controllers
             {
                 ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
             }
-            //Student student = db.Students.Find(id);
-            var student = new Person();
-            if (student == null)
+
+            var person = db.People.Find(id);
+            if (person == null)
             {
                 return HttpNotFound();
             }
-            return View(student);
+            return View(person);
         }
 
-        // POST: Student/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
         {
-            //Student student = db.Students.Find(id);
-                //db.Students.Remove(student);
-                //db.SaveChanges();
+            var person = db.People.Find(id);
+            db.People.Remove(person);
+            db.SaveChanges();
 
             return RedirectToAction("Index");
         }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                //db.Dispose();
+                db.Dispose();
             }
             base.Dispose(disposing);
         }

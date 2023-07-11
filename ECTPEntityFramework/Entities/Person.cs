b317bebf412c939a374cc9f82715875f93ed3651
David Houchens <david.houchens@lakeshore.com>
2023-07-11 13:21:26 -0400

    Remove person properties

--- ECTPEntityFramework/Entities/Person.cs
+++ ECTPEntityFramework/Entities/Person.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace ECTPEntityFramework.Entities
+﻿namespace ECTPEntityFramework.Entities
 {
     public class Person
     {
-        public int PersonId { get; set; }
-
-        [Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string LastName { get; set; }
-
-        public DateTime BirthDate { get; set; }
     }
 }
